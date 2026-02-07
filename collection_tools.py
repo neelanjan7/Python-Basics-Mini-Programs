acfeeee092936597bacadacb79ebf0fcf45189f0
Neelanjan Dalvi <neelanjandalvi09@gmail.com>
2026-02-08 00:11:25 +0530

    Demonstrate list, tuple, and set operations
    Added lists, tuples, and sets of band names with various operations.

--- collection_tools.py
+++ collection_tools.py
@@ -1,1 +1,20 @@
+bands_1 = ["The Strokes", "Maroon 5", "Imagine Dragons", "Tame Impala", "OneRepublic", "Arctic Monkeys"]
+print(bands_1)
 
+bands_2 = ("Maroon 5", "Imagine Dragons", "Vacations", "One Direction", "The neighbourhood")
+print(bands_2)
+
+bands_3 = {"The Strokes", "Maroon 5", "Imagine Dragons", "Tame Impala", "OneRepublic", "The Strokes", "Tame Impala"}
+print(bands_3)
+
+bands_1.append("Savage Garden")
+print(bands_1)
+
+bands_1.remove("OneRepublic")
+print(bands_1)
+
+print (len(bands_2))
+
+print (bands_2[0])
+
+print (bands_3 - {"The Strokes", "Tame Impala"})    
