6c32dcdfb141577e5dd6abd38fe8d6b6393f51cc
Neelanjan Dalvi <neelanjandalvi09@gmail.com>
2026-02-09 23:11:55 +0530

    Add character dictionary with various operations

--- dictionary_basics.py
+++ dictionary_basics.py
@@ -1,1 +1,18 @@
+character = {'name' : 'K', 'age' : 35, 'city' : 'Los Angeles', 'job' : ['Blade Runner', 'LAPD'] }
+print(character['job'])
+
+print(character.get('height', 'Height not found'))
+
+character['phone'] = '9334423333'
+
+
+character.update({'age' : 36, 'phone' : '1234-4321'})
+print(character.keys())
+
+for value, key in character.items():
+    print(key, ":" , value)
+
+if character ['age'] >= 18:
+    print("K")
+else:   print("Not K")
 
