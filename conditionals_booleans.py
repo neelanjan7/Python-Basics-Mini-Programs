177398a11f2651b1acf503ccf82385683992c2d0
Neelanjan Dalvi <neelanjandalvi09@gmail.com>
2026-02-12 12:21:56 +0530

    Add conditional checks for user login and language

--- conditionals_booleans.py
+++ conditionals_booleans.py
@@ -2,9 +2,31 @@ language = 'java'
 
 if language == 'python':
     print('Language is Python')
+
 elif language == 'java':
     print('Language is Java')
 
 else:
     print ('no match')
+
+
+
+
+user = 'admin'
+logged_in = False
+if user == 'admin' and logged_in :
+    print('Admin Page')
+else :
+    print('Bad creds')
+
+if user == 'admin' or logged_in :
+    print('Admin Page')
+else :
+    print('Bad creds')
+
+if not logged_in :
+    print ('Please log in')
+else : 
+    print ('Welcome')
+
 
