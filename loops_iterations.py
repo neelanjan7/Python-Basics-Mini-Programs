fe6e728b967c98eede243b7053a6ab725a30cee7
Neelanjan Dalvi <neelanjandalvi09@gmail.com>
2026-02-15 22:55:00 +0530

    Add loops and iterations examples in loops_iterations.py

--- loops_iterations.py
+++ loops_iterations.py
@@ -1,1 +1,20 @@
+nums = [1, 2, 3, 4, 5]
+
+for num in nums:
+    if num == 4:
+        print('Found!')
+        break
+    print(num)
+
+for num in nums:
+    for letter in 'ab':
+        print (nums, letter )
+
+x = 0
+
+while x < 10:
+    if x == 8:
+        break
+    print (x)
+    x += 1
 
