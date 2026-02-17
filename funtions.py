2f73d02e015182ee1c203507c78c0893a5e685b1
Neelanjan Dalvi <neelanjandalvi09@gmail.com>
2026-02-17 23:15:26 +0530

    Add hello_func and student_info functions

--- funtions.py
+++ funtions.py
@@ -1,1 +1,16 @@
+def hello_func(greeting, name = 'you' ):
+    return '{}, {}.'.format(greeting, name)
+
+print (hello_func('Hi'))
+
+
+
+def student_info(*args, **kwargs):
+    print (args)
+    print (kwargs)
+
+courses = ['Math', 'Arts']
+info = {'name' :'John', 'age' : 25 }
+
+student_info(*courses, **info)
 
