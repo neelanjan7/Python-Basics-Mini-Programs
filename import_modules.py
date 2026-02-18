21bba3ffd2a724d9a176e368b19f2f322f5218cd
Neelanjan Dalvi <neelanjandalvi09@gmail.com>
2026-02-18 23:40:03 +0530

    Add multiple imports and print statements

--- import_modules.py
+++ import_modules.py
@@ -1,1 +1,19 @@
+import math
+rads = math.radians(90)
+print(rads)
 
+import datetime
+today = datetime.date.today()
+print(today)
+
+import calendar
+print(calendar.isleap(2017))
+
+import random
+subjects = ['Japanese', 'Arts', 'Chemistry', 'English', 'CompSci', 'Math']
+random_subject = random.choice(subjects)
+print(random_subject)
+
+import os
+print(os.getcwd())
+print(os.__file__)
