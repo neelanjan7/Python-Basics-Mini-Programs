049cd6efbe59a9a920bbf60dbb4ac46a2346c2a1
Neelanjan Dalvi <neelanjandalvi09@gmail.com>
2026-05-09 23:34:50 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+num1 = int (input( 'first no tell yawr:' ))
+
+
+
+num2 = int (input( '2nd no tell na yawr:' ))
+
+
+operator = input( "in nos ke sath kya karna chaaate ho yawr:")
+if operator == '+' :
+    print(num1 + num2)
+else :
+    op2 = input("what do to ")
